b0114b33129bc41166d41d723446654cc57bf0c5
hostem17 <vjjais17@gmail.com>
2022-02-17 12:33:45 +0530

    Header done, Assets imported

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1> Hello World!</h1>
 
       {/* Header */}
+      <Header />
       {/* Main */}
       {/* Footer */}
     </div >
